a0d68e59edcd4501860e561cc28b25c32e6f7fda
alilinykh <aililnykh@dtcc.edu>
2019-05-21 17:17:35 -0400

    Lab too large too small

--- Main.java
+++ Main.java
@@ -1,33 +1,58 @@
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Alex Ilinykh)
+ * @version (05/21/2019)
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
+    int userNum;
     public Main()
     {
-        // initialise instance variables
-        x = 0;
+    //declare variables     
+    int userNum;
+    int prevNum = 0;
+    int counter = 1;
+    Scanner userInput = new Scanner(System.in);
+    Random rand = new Random();
+    //create random num
+    int misteryNum  = rand.nextInt(11);
+    System.out.println("Please enter number from 1 to 10");
+    userNum = userInput.nextInt();
+    while (userNum >= 11 || userNum <= 0) {
+        System.out.println("Please enter number from 1 to 10");
+        userNum = userInput.nextInt();
     }
+    while(userNum != misteryNum) {
+        
+        if (userNum != prevNum) {
+            prevNum = userNum;
+            counter++;
+        }
+        if(userNum > misteryNum) {
+            System.out.println("Too large");
+            userNum = userInput.nextInt();
+                while (userNum >= 11 || userNum <= 0) {
+                    System.out.println("Please enter number from 1 to 10");
+                    userNum = userInput.nextInt();
+                }
+        }
+        else if(userNum < misteryNum) {
+            System.out.println("Too small");
+            userNum = userInput.nextInt();
+                while (userNum >= 11 || userNum <= 0) {
+                    System.out.println("Please enter number from 1 to 10");
+                    userNum = userInput.nextInt();
+                }
+        
+        }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
+    System.out.println("Correct guess");
+    System.out.println("number of guesses: " + counter);
+  }
 }
+
